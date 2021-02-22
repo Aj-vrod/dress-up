e9572bd027d670d466b0c29d5ce7d0c9a7f7c282
Deborah Sillmann <deborahsillmann@gmx.de>
2021-02-22 19:32:08 +0100

    add first name, last lame and address, validation and dependencies to user model. Deleted dependent: :destroy from dresses

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -1,6 +1,7 @@
 class Dress < ApplicationRecord
   has_many :bookings
-  belongs_to :user, dependent: :destroy
+  # deleted , dependent: :destroy
+  belongs_to :user
 
   validates :title, :description, :category, :photo, :color, presence: true
   validates :price, presence: true, numericality: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # validations: all columns must be filled
+  validates :first_name, :last_name, :address, presence: true
+  # dependencies
+  has_many :dresses, dependent: :destroy
 end
