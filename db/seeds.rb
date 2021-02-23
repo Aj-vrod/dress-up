165698690499994fd60973c0a69694a790ec6bf8
Deborah Sillmann <deborahsillmann@gmx.de>
2021-02-23 09:20:52 +0100

    add avatar to user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,22 @@ require 'faker'
 # delete old stuff from db
 Dress.destroy_all
 puts 'Shredding old dresses...'
+User.destroy_all
+puts 'Kicking users...'
+
+# create new users
+10.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    encrypted_password:Faker::Internet.password(min_length: 8),
+    nickname: Faker::Internet.username,
+    address: Faker::Address.street_address
+    #avatar: Faker::Avatar.image
+    )
+  puts "User created!"
+end
 
 # types of dresses
 sample_categories = ['Bridal Dresses', 'Bridesmaids Dresses', 'Casual Dresses', 'Going Out Dresses', 'Occasion Dresses', 'Summer Dresses', 'Work Dresses']
@@ -15,7 +31,8 @@ sample_categories = ['Bridal Dresses', 'Bridesmaids Dresses', 'Casual Dresses', 
     price: Faker::Commerce.price,
     category: sample_categories.sample,
     color: Faker::Color.color_name ,
-    photo: 'https://www.fillmurray.com/640/360'
+    photo: 'https://www.fillmurray.com/640/360',
+    user: User.sample
     )
   puts "💃 Adding #{dress.title}..."
 end
