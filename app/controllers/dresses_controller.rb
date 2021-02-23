dc8dccbfbddde5cbbb0963c9ece2b5563c5344fa
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-02-23 17:22:31 +0100

    setup done

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -1,16 +1,18 @@
 class DressesController < ApplicationController
   before_action :set_params, only: [:show, :destroy]
   def index
-    @dresses = Dress.all
+    @dresses = policy_scope(Dress.all)
   end
-  
+
   def new
     @dress = Dress.new
+    authorize @dress
   end
-  
+
   def create
     @dress = Dress.new(dress_params)
     @dress.user = current_user
+    authorize @dress
     if @dress.save!
       redirect_to dress_path(@dress)
     else
@@ -19,18 +21,20 @@ class DressesController < ApplicationController
   end
 
   def show
+    authorize @dress
   end
-  
+
   def destroy
     @dress.destroy
     redirect_to dresses_path
   end
 
   private
+
   def dress_params
     params.require(:dress).permit(:title, :description, :photo, :color, :category, :price)
   end
-  
+
   def set_params
     @dress = Dress.find(params[:id])
   end
