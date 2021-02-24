e852fa3767ab7f4c9dd456d570d8f522de2072f0
Davinia Tosco Abreu <daviniatosco@gmail.com>
2021-02-24 11:15:30 +0000

    create view bookings new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,8 @@
 class BookingsController < ApplicationController
   def new
     @dress = Dress.find(params[:dress_id])
+    authorize @dress
     @booking = Booking.new
+    @user = current_user
   end
 end
