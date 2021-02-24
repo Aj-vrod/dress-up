6e409fd76d2244bb0486957c09d523632d6b032a
Davinia Tosco Abreu <daviniatosco@gmail.com>
2021-02-24 13:32:57 +0000

    create action in bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,4 +5,22 @@ class BookingsController < ApplicationController
     @booking = Booking.new
     @user = current_user
   end
+
+  def create
+    @dress = Dress.find(params[:dress_id])
+    @booking = Booking.new(booking_params)
+    @booking.dress = @dress
+    if @booking.save
+      redirect_to dress_path(@booking)
+    else
+      @user = User.new
+      render ""
+    end
+  end
+
+  private
+
+  def booking_params
+
+  end
 end
