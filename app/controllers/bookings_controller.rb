e57fc8f88f46e93e8d0c751b97aad7c8cfa3fab3
Deborah Sillmann <deborahsillmann@gmx.de>
2021-02-24 14:53:13 +0100

    index for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   def index
-    @booking = policy_scope(Booking)
+    @bookings = policy_scope(Booking)
   end
 
   def new
@@ -13,8 +13,8 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.dress = Dress.find(params[:dress_id])
-    if @booking.save
-      redirect_to dresses_path
+    if @booking.save!
+      redirect_to bookings_path
     else
       redirect_to dresses_path(@dress)
     end
