1fd1c6d7e6caa622ea41113e055d844d05bae8b4
Deborah Sillmann <deborahsillmann@gmx.de>
2021-02-24 12:20:52 +0100

    some changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,15 @@
 class BookingsController < ApplicationController
+  def index
+    @booking = policy_scope(Booking)
+  end
+
   def new
     @dress = Dress.find(params[:dress_id])
     @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    authorize @booking
   end
 end
