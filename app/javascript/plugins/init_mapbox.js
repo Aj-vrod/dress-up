2b7514b71f3286364f2f02e09bd6ade9f71a63f2
Debbie <sepp.der.bauer@alpenjodel.de>
2021-02-25 17:05:42 +0100

    added custom map style

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,7 +13,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/ammyvare/ckll1vrvz0h8g17ppqurhprd0'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
